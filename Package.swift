33d3bd751e9ee906709c27ec6fd3ef60ad8f765b
Eugene Mozharovsky <mozharovsky@live.com>
2017-08-13 20:02:38 +0300

    Using local `mysql-driver` repo

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "MySQLProvider",
     dependencies: [
         // MySQL driver for Fluent
-        .Package(url: "https://github.com/vapor/mysql-driver.git", majorVersion: 2),
+        .Package(url: "https://github.com/mozharovsky/mysql-driver.git", majorVersion: 2),
         // A provider for including Fluent in Vapor applications
         .Package(url: "https://github.com/mozharovsky/fluent-provider.git", majorVersion: 1),
         // A web framework and server for Swift that works on macOS and Ubuntu.
